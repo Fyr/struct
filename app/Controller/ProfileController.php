0e6e28bbf2630fcdbab1aaa9d562f3dd229f9c8f
Fyr <fyr.work@gmail.com>
2014-11-11 05:57:58 +0200

    Сделал вывод аватара для главной страницы

--- app/Controller/ProfileController.php
+++ app/Controller/ProfileController.php
@@ -36,4 +36,47 @@ class ProfileController extends SiteController {
 		$this->set('user', $this->ChatUser->getUser($id));
 	}
 	
+	public function getAvatar() {
+		$this->autoRender = false;
+		$id = Hash::get($this->profile, 'Media.id');
+		if (!$id) {
+			header('Content-type: image/jpg');
+			echo file_get_contents('/var/www/html/img/no-photo.jpg');
+			exit;
+		}
+		
+		$this->loadModel('Media.Media');
+		
+		$this->PHMedia = $this->Media->getPHMedia();
+		$media = Hash::get($this->profile, 'Media');
+		$type = $media['object_type'];
+		$size = 'thumb90x90';
+		$filename = $media['file'].$media['ext'];
+		
+		$media['ext'] = str_replace('.', '', $media['ext']);
+		
+		$fname = $this->PHMedia->getFileName($type, $id, $size, $filename);
+		if (file_exists($fname)) {
+			header('Content-type: image/'.$media['ext']);
+			echo file_get_contents($fname);
+			exit;
+		}
+		
+		App::uses('Image', 'Media.Vendor');
+		$image = new Image();
+		$image->load($this->PHMedia->getFileName($type, $id, null, $media['file'].'.'.$media['ext']));
+		$image->thumb(90, 90);
+		
+		if ($media['ext'] == 'jpg') {
+			$image->outputJpg($fname);
+			$image->outputJpg();
+		} elseif ($media['ext'] == 'png') {
+			$image->outputPng($fname);
+			$image->outputPng();
+		} else {
+			$image->outputGif($fname);
+			$image->outputGif();
+		}
+		exit;
+	}
 }
